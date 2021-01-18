7c9c1f56c1cd2b640896d252f7a61b1957f56a8c
anushkasinghal11 <singhalanushka.1999@gmail.com>
2021-01-18 17:23:10 +0530

    connected mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 import express from 'express'
+import bodyparser from 'body-parser'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
 const app=express();
-const Port=3000;
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
+
+mongoose.Promise=global.Promise;
+
+mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true,useUnifiedTopology: true }).then(()=>
+    {console.log("DB is connected");}    
+).catch((err)=>{
+    console.log(err);
+    process.exit();
+})
+
+const Port=process.env.PORT||3000;
+
 app.listen(Port,(err)=>{
     if(err) console.log(err);
     else console.log(`port is live at ${Port}`);
