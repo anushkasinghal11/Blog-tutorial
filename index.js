cc60db0dc6c6897e11383bb05d0e5ae218ee205d
anushkasinghal11 <singhalanushka.1999@gmail.com>
2021-01-23 18:04:50 +0530

    cors

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import express from 'express'
 import bodyparser from 'body-parser'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-
+import cors from 'cors'
 dotenv.config();
 
 const app=express();
-
+app.use(cors())
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
 
